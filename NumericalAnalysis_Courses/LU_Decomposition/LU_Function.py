6e29a47256f2d9d51e04a403494cce188bbf86fb
Jacky <1710915537@qq.com>
2021-04-14 13:30:25 +0800

    NumericalAnalysis Courses.

---
+++ NumericalAnalysis_Courses/LU_Decomposition/LU_Function.py
@@ -0,0 +1,47 @@
+# Created By Jacky on 2021/4/14
+
+
+import numpy as np
+
+def LU_D(A):
+    n = len(A)
+    L = np.eye(n)
+    U = A
+    for i in range(n):
+        for k in range(i+1,n):
+            for j in range(i+1,n):
+                fac = -U[k,i] / U[i,i]
+                U[k,j] = U[k,j] + U[i,j] * fac
+            aux = np.eye(n)
+            aux[k,i] = aux[k,i] - aux[i,i] * fac
+            L = np.dot(L,aux)
+            U[k,i] = 0
+
+    return L ,U         # 返回 矩阵 L 和 U
+
+def LU(A,b):
+    n = len(b)
+    L , U = LU_D(A)
+
+    print("L矩阵 : \n" , L  , "\n")
+    print("U矩阵 : \n" , U , "\n")
+
+    # 求解 Lc = b
+    c = np.zeros((n,1))
+    c[0] = b[0]
+    for i in range(1,n):
+        sum = 0
+        for j in range(0,n-1):
+            sum += L[i,j] * c[j]
+        c[i] = (b[i]-sum)
+
+    # 求解 Ux = c
+    x = np.zeros((n,1))
+    x[n-1] = c[n-1] / U[n-1,n-1]
+    for i in range(n-1,-1,-1):
+        sum = 0
+        for j in range(i+1,n):
+            sum += U[i,j] * x[j]
+        x[i] = (c[i]-sum) / U[i,i]
+
+    return x
---
+++ NumericalAnalysis_Courses/LU_Decomposition/Test.py
@@ -0,0 +1,24 @@
+# Created By Jacky on 2021/4/14
+
+
+
+import numpy as np
+from NumericalAnalysis_Courses.LU_Decomposition import LU_Function
+
+def Test(A,b):
+    x = LU_Function.LU(A,b)
+    print("解得 X : ")
+    print(x)
+
+
+if __name__ == '__main__':
+
+    # Example1:
+    A1 = np.mat([[1,2,-1],[2,1,-2],[-3,1,1]])
+    b1 = np.mat([3,3,-6]).T
+    Test(A1,b1)
+
+    # Example2:
+    A2 = np.mat([[1,2,3],[2,5,2],[3,1,5]])
+    b2 = np.mat([14,18,20]).T
+    Test(A2,b2)
